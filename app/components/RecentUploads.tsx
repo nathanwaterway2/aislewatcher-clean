fe9fb1eb8388c712a8d7997aa55293fa8fc6c950
nathanwaterway2 <nathanwaterway2@gmail.com>
2026-05-20 13:56:30 -0400

    fix: cast Supabase data to Upload[] to fix TypeScript type error

--- app/components/RecentUploads.tsx
+++ app/components/RecentUploads.tsx
@@ -43,7 +43,7 @@ export default function RecentUploads() {
       if (error) {
         console.error(error)
       } else {
-        setUploads(data || [])
+        setUploads((data as Upload[]) || [])
       }
 
       setLoading(false)
